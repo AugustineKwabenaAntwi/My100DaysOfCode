d0eacf4a66660fb40b17f19388e2f1c8d3421bbb
AugustineKwabenaAntwi <antwigoodness@gmail.com>
2024-03-10 07:03:13 +0000

    Update main.py

--- Day4/Exercise2/main.py
+++ Day4/Exercise2/main.py
@@ -4,20 +4,22 @@ stock_prices = {
     "mtl":[234,180,160]
     }
 
-choice = input("Enter the operation you want\nprint\nadd\n\n")
+choice = input("Enter the operation you want\nprint\nadd\n\n").strip()
 
-if choice == 'print':
+if choice.lower() == 'print':
     for key,value in stock_prices.items():
          avg=sum(value)/len(value)
          print(f'{key}==> {value}==> avg: {round(avg,2)}')
          avg = 0
 
-elif choice =='add':
-     new_stock =  input("Enter name of stock\n\n")
-     price = int(input("Enter the price of the stock\n\n"))
+elif choice.lower() =='add':
+     new_stock =  input("Enter name of stock\n\n").strip()
+     price = int(input("Enter the price of the stock\n\n").strip())
      if new_stock in stock_prices:
           #work on adding new this may be wrong
+          stock_prices[new_stock].append(price)
+          print(stock_prices)
+     else:
           stock_prices[new_stock]=price
-     else:          
-
-                   
+          print(stock_prices)
+else: print(f'Unsupported operation: {choice}')                  
